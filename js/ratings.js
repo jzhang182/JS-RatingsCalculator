ab90958c61c4825b33a0eceb1b72c956ddc61238
Jialong ZHANG <JZhang182@slb.com>
2020-10-29 13:57:17 +0800

    2

--- js/ratings.js
+++ js/ratings.js
@@ -7,17 +7,17 @@ function collect_ratings() {
     let rating = 0;
     const elements = document.querySelectorAll('.rating');
     elements.forEach(element => {
-        rating = parseInt(element.Id.replace('star', ''));
+        rating = parseInt(element.id.replace('star', ''));
         ratings.count += parseInt(element.value);
         ratings.sum += parseInt(element.value) * rating;
     });
     if (ratings.count != 0) {
         ratings.average = ratings.sum / ratings.count;
     }
-    return ratings.average
+    return ratings
 }
 
 document.addEventListener('change', () => {
     const ratings = collect_ratings();
-    document.querySelector('#average').value = ratings.average;
+    document.querySelector('#average').value = ratings.average.toFixed(2);
 })
